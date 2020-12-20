a3e6d6eb5c309403dde34869b66d887b6f12e66c
apollo78124 <98apollo@naver.com>
2020-12-19 20:54:08 -0800

    User controller

---
+++ aspdotnetCRUDTutorial/ReactASPDotNetIntegration/ReactASPDotNetIntegration/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReactASPDotNetIntegration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
